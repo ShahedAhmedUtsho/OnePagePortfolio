e9f55e0df657e8bc7915e856e19979337363fd39
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-06-21 22:16:17 +0600

    header color currection

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
 
     return (
         <>
-            <div className={`w-full globalBG fixed left-0 transition-transform z-[999] duration-300 ${visible ? "top-0" : "-top-20"} right-0 m-auto border-b border-slate-500 bg-indigo-100 dark:bg-gray-950 leading-6 min-h-[72px] md:px-10 flex font-input-sans items-center`}>
+            <div className={`w-full globalBG fixed left-0 transition-transform z-[999] duration-300 ${visible ? "top-0" : "-top-20"} right-0 m-auto border-b border-slate-500 bg-indigo-100 dark:bg-slate-900 leading-6 min-h-[72px] md:px-10 flex font-input-sans items-center`}>
                 <div className='flex gap-3 items-center w-full h-full'>
                     <div className='logo flex items-center'>
                         <Link to="/" className='font-input-sans text-3xl dark:text-slate-400'>
@@ -63,7 +63,7 @@ const Header = () => {
                     <div className='flex lg:justify-between justify-end items-center w-full'>
                         <nav className='capitalize hidden lg:block'>
                             {["project", "templates", "components", "blog"].map((a, index) => (
-                                <NavLink className='text-sm text-left align-bottom px-5 py-2 text-slate-400 hover:text-slate-300 leading-5' key={index} to={`/${a}`}>
+                                <NavLink className='text-sm text-left align-bottom px-5 py-2 dark:text-slate-400 text-slate-700 dark:hover:text-slate-200  hover:text-slate-900 hover:font-black leading-5' key={index} to={`/${a}`}>
                                     <span className='font-input-sans capitalize'>{a}</span>
                                 </NavLink>
                             ))}
@@ -72,14 +72,14 @@ const Header = () => {
                             <button onClick={handleMood} className='px-2 text-base dark:text-indigo-300'>
                                 {dark ? <Sun size={18} /> : <Moon size={18} />}
                             </button>
-                            <button className='ghostBtn btn self-end hidden lg:inline-block text-slate-400 font-input-sans text-sm'>
+                            <button className='ghostBtn btn self-end hidden lg:inline-block dark:text-slate-400 text-slate-700 hover:bg-indigo-400 hover:bg-opacity-30 dark:hover:text-slate-200  hover:text-slate-900 hover:font-black font-input-sans text-sm'>
                                 Log in
                             </button>
-                            <button className='btn border hidden lg:inline-block border-indigo-300 text-indigo-300 font-input-sans text-sm'>
+                            <button className='ghostBtn btn self-end hidden lg:inline-block dark:text-indigo-300 text-indigo-800 hover:bg-opacity-10 hover:bg-indigo-400 dark:hover:text-slate-200  hover:text-slate-900 hover:font-black font-input-sans text-sm'>
                                 Join
                             </button>
-                            <button onClick={toggleBar} className='lg:hidden text-indigo-300'>
-                                {isOpen ? <X size={28} className='text-white' /> : <AlignJustify size={28} className='text-white' />}
+                            <button onClick={toggleBar} className='lg:hidden text-indigo-800 dark:text-indigo-300 '>
+                                {isOpen ? <X size={28} className='dark:text-white text-black' /> : <AlignJustify size={28} className='dark:text-white text-black' />}
                             </button>
                         </div>
                     </div>
@@ -87,18 +87,20 @@ const Header = () => {
             </div>
 
             {isOpen && (
-                <div className='w-full flex flex-col lg:hidden fixed bg-slate-800 globalBG pt-20 h-screen'>
+                <div className='w-full flex flex-col lg:hidden fixed bg-indigo-100 dark:bg-slate-900  globalBG pt-20 h-screen'>
                     {["project", "templates", "components", "blog"].map((a, index) => (
-                        <NavLink onClick={toggleBar} className='text-sm text-left align-bottom px-5 py-2 text-slate-400 hover:text-slate-300 leading-5' key={index} to={`/${a}`}>
+                        <NavLink onClick={toggleBar} className='text-sm text-left align-bottom px-5 py-2 ark:text-slate-400 text-slate-700 dark:hover:text-slate-200   hover:text-slate-900 hover:font-black  leading-5' key={index} to={`/${a}`}>
                             <span className='font-input-sans capitalize'>{a}</span>
                         </NavLink>
                     ))}
-                    <button onClick={toggleBar} className='ghostBtn btn mx-5 my-2 text-slate-400 font-input-sans text-sm'>
-                        Log in
-                    </button>
-                    <button onClick={toggleBar} className='btn border mx-5 my-2 border-indigo-300 text-indigo-300 font-input-sans text-sm'>
-                        Join
-                    </button>
+                 
+                   <button className='ghostBtn btn mx-5 my-2    dark:text-slate-400 text-slate-700 hover:bg-indigo-400 hover:bg-opacity-30 dark:hover:text-slate-200  hover:text-slate-900 hover:font-black font-input-sans text-sm'>
+                                Log in
+                            </button>
+                            <button className='ghostBtn btn mx-5 my-2   dark:text-indigo-300 text-indigo-800 hover:bg-opacity-10 hover:bg-indigo-400 dark:hover:text-slate-200  hover:text-slate-900 hover:font-black font-input-sans text-sm'>
+                                Join
+                            </button>
+                 
                 </div>
             )}
         </>
